67d998619e0627790192c3e8c11b4131d4b2fe49
Victoria Finney <vfinney2@gmail.com>
2017-06-14 18:03:26 -0400

    Add modified landing.js and index.html

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,25 +1,31 @@
-var animatePoints = function () {
-  "use strict";
-  var points = document.getElementsByClassName('point'),
-     
-    revealFirstPoint = function () {
-      points[0].style.opacity = 1;
-      points[0].style.transform = "scaleX(1) translateY(0)";
-      points[0].style.msTransform = "scaleX(1) translateY(0)";
-      points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    },
+var pointsArray = document.getElementsByClassName('point');
  
-    revealSecondPoint = function () {
-      points[1].style.opacity = 1;
-      points[1].style.transform = "scaleX(1) translateY(0)";
-      points[1].style.msTransform = "scaleX(1) translateY(0)";
-      points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    },
- 
-    revealThirdPoint = function () {
-      points[2].style.opacity = 1;
-      points[2].style.transform = "scaleX(1) translateY(0)";
-      points[2].style.msTransform = "scaleX(1) translateY(0)";
-      points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+var revealPoint = function (point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+
+var animatePoints = function(points) {
+    
+  forEach(points, revealPoint);
+
 };
+
+
+window.onload = function () {
+  
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0]; 
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  
+  window.addEventListener('scroll', function (event) {
+             if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+    
+  });
+}
